cf3ecbe9fe827b49133bea266d007795ab643d21
mahmoudg.dev <mahomudg.dev@gmail.com>
2025-07-25 15:54:09 +0300

    add portfolio

---
+++ Mahmoud Gamal/Week 4 ( Portfolio )/README.md
@@ -0,0 +1,40 @@
+# Mahmoud Gamal Portfolio
+
+Welcome to my personal portfolio website! This project showcases my skills, experience, and projects as a computer programming student and MERN stack developer.
+
+## Features
+
+- Responsive design for all devices
+- About me section with personal details
+- Downloadable CV
+- Navigation bar for easy access to all pages
+- Social media links
+- Footer with inspirational quote
+
+## Screenshots
+
+### Home Page
+![Home Page](screenshots/home.png)
+
+### About Page
+![About Page](screenshots/about.png)
+
+### Resume Page
+![Projects Page](screenshots/skills.png)
+
+### Services Page
+![Projects Page](screenshots/services.png)
+
+### Contact Page
+![Projects Page](screenshots/contact.png)
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- JavaScript
+- Font Awesome
+- Google Fonts
+
+
+
---
+++ Mahmoud Gamal/Week 4 ( Portfolio )/js/skills.js
@@ -0,0 +1,40 @@
+let skillstitle = ["css", "html", "Bootstrap", "React.js", "Node.js", "express", "python", "js", "Typescript","Postgres","MongoDBb","cpp", "java"];
+let skillsValue = [80, 90, 80, 70, 60, 50, 90, 80, 80,90,70,20, 40];
+
+
+/*
+<div class="skill">
+          <span class="skill-name">skill</span>
+          <span class="value">number</span>
+          <div class="process">
+            <div class="process-bar"></div>
+          </div>
+        </div>
+ */
+
+for (let index = 0; index < skillstitle.length; index++) {
+  var div = document.createElement("div");
+  div.className = "skill";
+  var skillName = document.createElement("span");
+  skillName.className = "skill-name";
+  var skillValue = document.createElement("span");
+  skillValue.className = "value";
+  var process = document.createElement("div");
+  process.className = "process";
+  var processBar = document.createElement("div");
+  processBar.className = "process-bar";
+  process.appendChild(processBar);
+  div.appendChild(skillName)
+  div.appendChild(skillValue)
+  div.appendChild(process)
+  skillName.innerHTML = `${skillstitle[index]}`;
+  skillValue.innerHTML = `${skillsValue[index]}%`;
+  processBar.style.width = `${skillsValue[index]}%`;
+  process.appendChild(processBar);
+  div.appendChild(skillName)
+  div.appendChild(skillValue)
+  div.appendChild(process)
+  document.querySelector(".skills .row").appendChild(div)
+
+}
+
