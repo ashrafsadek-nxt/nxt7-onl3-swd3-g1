78540ba13fd1c61b2308efc7f5e9df311d23a5ea
hajarhabib <hajartech20@gmail.com>
2025-08-03 14:57:40 +0300

    Delete Hagar Habib/week 6/js-third-Edited/dark mode toggle directory

--- Hagar Habib/week 6/js-third-Edited/dark mode toggle/script.js
+++
@@ -1,9 +0,0 @@
-const toggleSwitch = document.querySelector("#mode-toggle");
-
-toggleSwitch.addEventListener("change", function () {
-  if (this.checked) {
-    document.body.classList.add("dark-mode");
-  } else {
-    document.body.classList.remove("dark-mode");
-  }
-});
